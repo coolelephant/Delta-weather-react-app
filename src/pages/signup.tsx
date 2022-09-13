4ddcf645c19b2f6a57d7c29710ed4226e3696250
jaewoo <wjdwodn0722@naver.com>
2022-09-13 17:03:43 +0900

    feat : copyright, sign up 로고 추가
    - .gitmessage.txt 템플릿을 이용하여 작성
    - 하단 copyright 부분 추가
    - 상단 Sign up 제목 및 아이콘 추

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,13 +1,37 @@
 import { Button, Grid, Paper, TextField } from "@mui/material";
+import Avatar from '@mui/material/Avatar';
+import Link from '@mui/material/Link';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright '}
+      <Link color="inherit" href="https://mui.com/">
+        Delta Weather
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 export const SignUp = () => {
   return (
     <Grid container justifyContent={"center"} height={"100vh"}>
       <Grid item xs={12} md={5} component={Paper} elevation={4} square>
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
         <TextField fullWidth label={"email"} />
         <TextField fullWidth label={"password"} />
         <TextField fullWidth label={"name"} />
         <Button>회원가입</Button>
+      <Copyright sx={{ mt: 5 }} />
       </Grid>
     </Grid>
   );
